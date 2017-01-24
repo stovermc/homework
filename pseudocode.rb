d298654b22f3b37904def58dac704d52e40b572f
stovermc <stovermc@gmail.com>
2017-01-24 09:09:42 -0700

    Initial commit

---
+++ pseudocode.rb
@@ -0,0 +1,18 @@
+#1. We will know the problem is solved once our program can return the proper output that is dictated by specific input. Additionally, our program will close
+# after it has received two consecutive "GOODBYE!"
+
+#2.The sofware will be used in the terminal. Once the program is run, the user will be able to ask the software questions and receive less than desirable 
+#responses depending on the format of the quesions asked.
+
+#3.In this case, the most trivil possible case would be when the user does not provide any input and only hits the enter key.
+
+#4.Write program that takes input and based on input gives specific output.
+#Define variable goodbye tally, and user input and set both to zero.
+#Print "THANKS FOR CONTACTING BUSTER'S CHEESE STORE!"
+#While goodbye tally does not equal two, continue to receive input.
+#Get user input and convert to a string.
+#If user input is empty, then print "HELLO?!"
+#Else if user input does not equal user input.upcase, and the last element in the string is a ?, then print "I AM HAVING A HARD TIME HEARING YOU"
+#Else if user input equals user input.upcase, and the last element in the string is a ?, then print "NO, THIS IS NOT A PET STORE"
+#Else if user input equals "GOODBYE!", and goodbye tally is less than 1, add 1 to goodbye tally and print "ANYTHING ELSE I CAN HELP WITH?"
+#Else if user input is "GOODBYE!", and goodbye tally equals 1, then add one to the user input variable and print "THANK YOU FOR CALLING!"
